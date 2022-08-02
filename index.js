9eec73a5a7d04f6b6fe4039faf1c80a5a4e095c1
nivk <nivkman@gmail.com>
2022-08-02 23:54:00 +0300

    set basic express app

---
+++ index.js
@@ -0,0 +1,9 @@
+import express from 'express';
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`)
+})
