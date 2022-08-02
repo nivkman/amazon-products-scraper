3cc962c30b9462cafbf446d8942dee0a0fb0ea12
nivk <nivkman@gmail.com>
2022-08-03 00:04:26 +0300

    adding body-parser

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import express from 'express';
+import bodyParser from 'body-parser'
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.use('/api/v1', require('./routers/v1.js'));
 
 app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`)
