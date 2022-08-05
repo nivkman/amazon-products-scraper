510489b89626bc0b2eba9e5931433d0a64d430cb
nivk <nivkman@gmail.com>
2022-08-05 23:31:18 +0300

    adding page.goto

--- src/functions/scrape.js
+++ src/functions/scrape.js
@@ -1,5 +1,8 @@
-const scrapeController = async(req, res) => {
+import { openBrowser, closeBrowser, sleep, scrollDown, removeHtmlElement } from '../functions/browserHandler';
 
+const scrapeController = async(searchURL, regionZipCode, limit) => {
+    const [browser, page] = await openBrowser();
+    await page.goto(searchURL);
 }
 
 export default scrapeController;
