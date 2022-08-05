28bf0b35bbc9084f75d7180a67fcdee966741eaf
nivk <nivkman@gmail.com>
2022-08-05 23:26:20 +0300

    adding limit param

--- src/routers/v1.js
+++ src/routers/v1.js
@@ -1,10 +1,11 @@
 import express from 'express';
+import scrapeController from '../functions/scrape';
 const router = express();
 
-router.post('/scrape', (req, res) => {
-    const searchURL = req.body.searchURL;
-    const regionZipCode = req.body.regionZipCode;
-    res.json({ searchURL });
+router.post('/scrape', async(req, res) => {
+    const [searchURL, regionZipCode, limit] = await req.body;
+    await scrapeController(searchURL, regionZipCode, limit);
+    res.json({ status: "running" });
 })
 
 export default router;
